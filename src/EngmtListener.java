bfba4cdb59dd20fa84116a60795fc2c63fb227f2
Ethan Padden <ethanpadden@gmail.com>
2019-07-22 13:49:55 +0100

    Add other engmt listener class

---
+++ src/EngmtListener.java
@@ -0,0 +1,23 @@
+import com.google.gson.JsonParser;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import twitter4j.TwitterStream;
+import twitter4j.User;
+
+import java.io.FileReader;
+
+public class EngmtListener {
+    private User user;
+    private TwitterStream tweetStream;
+    private FileReader fileReader;
+    private DB db;
+    private DBCollection trackers;
+    private DBCollection hashtags;
+    private DBCollection other;
+
+    public EngmtListener(User user, DB db) {
+        this.user = user;
+        this.db = db;
+    }
+}
